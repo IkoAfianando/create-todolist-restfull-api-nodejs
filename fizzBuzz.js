a684e68b5b8d6e0fca0f2324218f4b53d2226bad
IkoAfianando <ikoafianando123@gmail.com>
2021-12-30 20:02:10 +0700

    Push

---
+++ fizzBuzz.js
@@ -0,0 +1,31 @@
+class FizzBuzz {
+    constructor(name) {
+        this.name = name;
+    }
+
+    fizzBuzz(data) {
+        for (let i = 1; i <= data; i++) {
+            const data1 = i % 3 === 0 && i % 5 === 0;
+            const data2 = i % 3 === 0;
+            const data3 = i % 5 === 0;
+            if(data1) {
+                console.log(`FizzBuzz ${i}`);
+            }else if(data2) {
+                console.log(`Fizz ${i}`);
+            }else if(data3) {
+                console.log(`Buzz ${i}`);
+            }else {
+                console.log(i);
+            }
+        }
+    }
+
+    sayHello(name) {
+        return `Hello ${name}, My Name is ${this.name}`;
+    }
+}
+
+
+const fizzBuzz = new FizzBuzz("Iko Afianando");
+fizzBuzz.fizzBuzz(1000);
+console.log(fizzBuzz.sayHello("Joko"));
