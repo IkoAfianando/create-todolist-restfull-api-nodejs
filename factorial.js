95347fb2842c7ae70f0eb7db728c332103d6ce9f
IkoAfianando <ikoafianando123@gmail.com>
2022-01-04 18:39:20 +0700

    Push

--- factorial.js
+++ factorial.js
@@ -2,12 +2,10 @@ function factorialTest(value) {
     if(value <= 0) {
         return 1;
     }
-
     let result = 1;
     for (let i = value; i >= 1; i--) {
         result *= i;
     }
-
     return result;
 }
 
